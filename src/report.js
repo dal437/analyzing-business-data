5624fae735f7932bdb9192e3b9194df800bf6bba
dalanao <dal437@nyu.edu>
2017-09-29 03:39:09 -0400

    Finished homework.

--- src/report.js
+++ src/report.js
@@ -1,25 +1,43 @@
 // report.js
 const yelpfunc = require('./yelpfunc.js');
-const request = require ('request')
+const request = require('request');
 
-request('https://foureyes.github.io/csci-ua.0480-fall2017-003/homework/02/086e27c89913c5c2dde62b6cdd5a27d2.json',
-"utf8", function(err, response, body){
-  if (err){
-    console.log("Sorry" + err);
+function subsequentfile(newurl){
+  return new Promise(resolve =>{
+    let url = newurl;
+  request('https://foureyes.github.io/csci-ua.0480-fall2017-003/homework/02/' + newurl,
+  "utf8", function(err, response, body){
+    if (err){
+      console.log("Sorry" + err);
+    }
+    else{
+      const stringarr = body.trim();
+      const newstringarr = stringarr.split('\n');
+
+      const resultsarr = [];
+
+      newstringarr.forEach(function(element){
+        resultsarr.push(JSON.parse(element.trim()));
+      });
+      console.log('https://foureyes.github.io/csci-ua.0480-fall2017-003/homework/02/086e27c89913c5c2dde62b6cdd5a27d2.json');
+      console.log(yelpfunc.processYelpData(resultsarr));
+      console.log(resultsarr[resultsarr.length - 1]);
+      url = resultsarr[resultsarr.length - 1].nextFile;
+    }
+    resolve(url);
+  });
+})
+
+.then(url => {
+  if (url){
+    return subsequentfile(url);
   }
-  else{
-    const stringarr = body.trim();
-    const newstringarr = stringarr.split('\n');
-
-    const resultsarr = [];
-
-    newstringarr.forEach(function(element){
-      resultsarr.push(JSON.parse(element.trim()));
-    });
-    console.log('https://foureyes.github.io/csci-ua.0480-fall2017-003/homework/02/086e27c89913c5c2dde62b6cdd5a27d2.json')
-    console.log(yelpfunc.processYelpData(resultsarr));
-  }
+  return null;
 });
+
+}
+
+subsequentfile('086e27c89913c5c2dde62b6cdd5a27d2.json');
 /*const fs = require('fs');
 
 fs.readFile("C:/Users/Diego/Documents/GitHub/dal437-homework02/tests/business.json", "utf8", function(err, data) {
